f8e8d03836fff48a48cce141076b7af5107b6a71
CoolOtaku <ericspz531@gmail.com>
2025-10-28 16:20:43 +0200

    first commit

---
+++ simply-poly/simly-poly.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: SimplyPoly
+Description: A lightweight multilingual plugin for WordPress that manages translations inside a single page using a visual translation builder.
+Version: 0.1
+Author: CoolOtaku
+License: GNU General Public License
+*/
+
+namespace SimplyPoly;
+
+if (!defined('ABSPATH')) exit;
+
+?>
